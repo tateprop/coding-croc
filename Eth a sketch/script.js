7e0cc2084d45a450393f53a1245a51b98aa1b91d
tateprop <yakovtajtelbaum@gmail.com>
2023-11-30 17:07:18 +0000

    Finished etch a sketch

---
+++ Eth a sketch/script.js
@@ -0,0 +1,21 @@
+
+
+function addGrid(n){
+    let gridParent = document.querySelector(".grid-container")
+    let gridElems = document.querySelectorAll(".grid-elem")
+    gridElems.forEach((elem) => elem.remove())
+    for (let i = 0; i < n**2; i++) {
+        let gridElem = document.createElement('div');
+        gridElem.classList.add('grid-elem');
+        gridElem.style.width = `${100/n}%`
+        gridElem.style.height = `${100/n}%`
+        gridElem.addEventListener('mouseover', () => gridElem.style.background="black")
+        gridParent.appendChild(gridElem)
+    }
+}
+function getValue(){
+    let elem = document.querySelector("#gridInput")
+    let gridSize = elem.value
+    elem.value = ""
+    addGrid(gridSize)
+}
