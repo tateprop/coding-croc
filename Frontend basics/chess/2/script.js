bbc54b5b8098f3f599e84263fa14d4f2eb8129f5
tateprop <yakovtajtelbaum@gmail.com>
2023-12-07 21:50:36 +0000

    Implemented main logic for chess 2

--- Frontend basics/chess/2/script.js
+++ Frontend basics/chess/2/script.js
@@ -1,18 +1,87 @@
 let boardSquares = []
 let pieceOrder = ["rook", "bishop", "knight", "queen","king","knight","bishop","rook"]
 let pieceMovement = {
-    "pawn" :{"moves" : [[1,0]], "bi" : false, "one": true},
-    "rook" : {"moves" : [[1,0]], "bi" : true, "one": false},
-    "bishop" : {"moves" : [[1,1]], "bi" : true, "one": false},
-    "knight" : {"moves" : [[2,1],[1,2]], "bi" : true, "one": true},
-    "queen" : {"moves" : [[1,0],[1,1]], "bi" : true, "one": false},
-    "king" : {"moves" : [[1,0],[1,1]], "bi" : true, "one": true},
+    "rook" : {"moves" : [[1,0],[0,1]], "one": false},
+    "bishop" : {"moves" : [[1,1]], "one": false},
+    "knight" : {"moves" : [[2,1],[1,2]], "one": true},
+    "queen" : {"moves" : [[1,0],[1,1],[0,1]], "one": false},
+    "king" : {"moves" : [[1,0],[1,1],[0,1]], "one": true},
+}
+const pieceMoveExpanded = {}
+const negatives = [[1,1], [-1,-1], [1,-1], [-1,1]];
+
+//This is ugly and gross but i couldnt get class variables to work
+let SELETED = ""
+/////////////
+
+// To Do
+// 1 game loop with turns
+// 2 fix pawn motion
+// 3 add check detection
+// 4 ceck mate detection
+
+function expandPieceMovement(){
+    for (const [key, value] of Object.entries(pieceMovement)){
+        let moveList = []
+        for (let i = 0; i < value["moves"].length; i++) {
+            let move = value["moves"][i];
+            for (let x = 0; x < 4; x++){
+                let currMove = [move[0]*negatives[x][0], move[1]*negatives[x][1]]
+                let loopsB = value["one"] ? 2 : 8
+                let subMoveList = []
+                for (let y = 1; y < loopsB; y++){
+                    subMoveList.push([currMove[0]*y, currMove[1]*y])
+                }
+                moveList.push(subMoveList)
+            }
+        }
+        pieceMoveExpanded[key] = moveList
+    }
 }
 class Piece{
-    constructor(pieceName, pieceColor){
+    constructor(pieceName, pieceColor, parent){
         this.pieceName = pieceName;
         this.pieceColor = pieceColor;
         this.hasMoved = false;
+        this.parent = parent
+    }
+    getMoves(){
+        //assume piece check has been made
+        SELETED = this
+        let index = this.parent.index
+        if (this.pieceName == "pawn"){
+            let flip = (this.pieceColor=="black") ? 1 : -1
+            let loops = this.hasMoved ? 1 : 2
+            let pawnMoves = []
+            for (let i = 0; i < loops; i++) {
+                pawnMoves.push([[0,1*flip*(i+1)]])
+            }
+            pieceMoveExpanded["pawn"] = pawnMoves
+        }
+        console.log(pieceMoveExpanded["pawn"])
+        let pieceDict = pieceMoveExpanded[this.pieceName]
+        for (let i = 0; i < pieceDict.length; i++) {
+            let moveTree = pieceDict[i];
+            for (let x = 0; x < moveTree.length; x++) {
+                let movePair = moveTree[x];
+                let newIndex = index + movePair[0] + movePair[1]*8
+                let newColumn = newIndex % 8
+                if (newIndex > 63 || newIndex < 0 || this.parent.column+movePair[0] !=newColumn){
+                    break
+                }
+                let newSquare = boardSquares[newIndex]
+                if (newSquare.piece){
+                    if (newSquare.piece.pieceColor !== this.pieceColor){
+                        boardSquares[newIndex].selected = true
+                    }
+                    break
+                }
+                else{
+                    boardSquares[newIndex].selected = true
+                }
+            }
+        }
+        
     }
 }
 class Board{
@@ -22,6 +91,7 @@ class Board{
         this.column = this.setColumn()
         this.color = this.setColor()
         this.piece = this.setPiece()
+        this.selected = false
     }
     setRow(){
         return Math.trunc(this.index/8)
@@ -34,23 +104,34 @@ class Board{
         return even ? "black" : "white"
     }
     setPiece(){
-        if ([0, 1, 6, 7].includes(this.row)){
+        /// add ,1 here
+        if ([0,1, 6, 7].includes(this.row)){
             let pieceColor = [0, 1].includes(this.row) ? "black" : "white"
             let pieceName = [6, 1].includes(this.row) ? "pawn" : pieceOrder[this.column]
-            let piece = new Piece(pieceName, pieceColor)
+            let piece = new Piece(pieceName, pieceColor, this)
             return piece
         }
         return null
     }
+    movePiece(){
+        //static
+        console.log(SELETED)
+        let prevIndex = SELETED.parent.index
+        boardSquares[prevIndex].piece = null
+        this.piece = SELETED
+        this.piece.parent = this
+        this.piece.hasMoved = true
+        SELETED = ""
+    }
 }
 function updateBoard(){
-    for (elem of document.getElementsByClassName("highlight")) {
+    for (elem of document.querySelectorAll(".highlight")) {
         elem.classList.remove("highlight");
     }
-    for (elem of document.getElementsByClassName("c-highlight")) {
+    for (elem of document.querySelectorAll(".c-highlight")) {
         elem.classList.remove("c-highlight");
     }
-    let imageList = document.querySelector("img");
+    let imageList = document.querySelectorAll("img");
     if (imageList){
         imageList.forEach(image => {
             image.remove()
@@ -66,6 +147,10 @@ function updateBoard(){
             pieceImage.className = "piece-image"
             boardSquareDiv.appendChild(pieceImage)
         }
+        if (square.selected){
+            let color = (piece) ? "c-highlight" : "highlight"
+            boardSquareDiv.classList.add(color)
+        }
     }
 }
 function generateBoard(){
@@ -75,18 +160,32 @@ function generateBoard(){
         let boardSquareDiv = document.createElement("div")
         boardSquareDiv.className = `board-square ${boardSquare.color}`
         boardSquareDiv.id = `square${i}`
+        boardSquareDiv.addEventListener("click", function (){clickBoard(i)})
         parent.appendChild(boardSquareDiv)
         boardSquares.push(boardSquare)
     }
     updateBoard()
 }
-function getMoves(index){
-    //assume piece check has been made
+
+function clickBoard(index){
     let square = boardSquares[index]
-    let piece = square.piece
-    let pieceDict = pieceMovement[piece.pieceName]
+    let skip = false
+    if (square.selected){
+        square.movePiece()
+        skip = true
+    }
+    for (let i = 0; i < boardSquares.length; i++) {
+        let element = boardSquares[i];
+        element.selected = false
+    }
+    if (square.piece && !skip){
+        square.piece.getMoves()
+        
+    }
+    updateBoard()
     
 }
 window.addEventListener("DOMContentLoaded", function(){
+    expandPieceMovement()
     generateBoard()
 }, false)
